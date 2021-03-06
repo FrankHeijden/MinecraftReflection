d5e03e4eb0b443b3827c21ceaa8f45fe012b55f0
Frank van der Heijden <F.N.M.vanderHeijden@student.tudelft.nl>
2021-03-06 16:31:12 +0100

    Implement MethodHandle's instead of reflection calls

--- src/main/java/dev/frankheijden/minecraftreflection/Reflection.java
+++ src/main/java/dev/frankheijden/minecraftreflection/Reflection.java
@@ -4,10 +4,10 @@ import dev.frankheijden.minecraftreflection.cache.NamedReflectionCacheTree;
 import dev.frankheijden.minecraftreflection.cache.ReflectionCacheTree;
 import dev.frankheijden.minecraftreflection.exceptions.MinecraftReflectionException;
 import dev.frankheijden.minecraftreflection.utils.ReflectionStringUtils;
-
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,20 +15,23 @@ import java.util.Map;
 public class Reflection {
 
     private final Class<?> clazz;
-    private final Map<String, Field> fieldMap;
-    private final NamedReflectionCacheTree<Method> methodTree;
-    private final ReflectionCacheTree<Constructor<?>> constructorTree;
+    private final Map<String, MethodHandle> fieldGetterMap;
+    private final Map<String, MethodHandle> fieldSetterMap;
+    private final NamedReflectionCacheTree<MethodHandle> methodTree;
+    private final ReflectionCacheTree<MethodHandle> constructorTree;
 
     public Reflection(Class<?> clazz) {
         this.clazz = clazz;
-        this.fieldMap = new HashMap<>();
+        this.fieldGetterMap = new HashMap<>();
+        this.fieldSetterMap = new HashMap<>();
         this.methodTree = new NamedReflectionCacheTree<>();
         this.constructorTree = new ReflectionCacheTree<>(null);
     }
 
     protected Reflection(Reflection reflection) {
         this.clazz = reflection.clazz;
-        this.fieldMap = reflection.fieldMap;
+        this.fieldGetterMap = reflection.fieldGetterMap;
+        this.fieldSetterMap = reflection.fieldSetterMap;
         this.methodTree = reflection.methodTree;
         this.constructorTree = reflection.constructorTree;
     }
@@ -48,9 +51,9 @@ public class Reflection {
     @SuppressWarnings("unchecked")
     public <R> R newInstance(Object... parameters) {
         try {
-            return (R) constructorTree.computeIfAbsent(getTypes(parameters), types -> getAccessibleConstructor(clazz, types)).newInstance(parameters);
-        } catch (IllegalAccessException | InstantiationException | InvocationTargetException ex) {
-            throw new MinecraftReflectionException(ex);
+            return (R) constructorTree.computeIfAbsent(getTypes(parameters), types -> getConstructorHandle(clazz, types)).invoke(parameters);
+        } catch (Throwable th) {
+            throw new MinecraftReflectionException(th);
         }
     }
 
@@ -59,26 +62,26 @@ public class Reflection {
         try {
             Class<?>[] parameterTypes = ClassObject.getTypes(classObjects);
             Object[] parameters = ClassObject.getObjects(classObjects);
-            return (R) constructorTree.computeIfAbsent(parameterTypes, types -> getAccessibleConstructor(clazz, types)).newInstance(parameters);
-        } catch (IllegalAccessException | InstantiationException | InvocationTargetException ex) {
-            throw new MinecraftReflectionException(ex);
+            return (R) constructorTree.computeIfAbsent(parameterTypes, types -> getConstructorHandle(clazz, types)).invoke(parameters);
+        } catch (Throwable th) {
+            throw new MinecraftReflectionException(th);
         }
     }
 
     @SuppressWarnings("unchecked")
     public <R> R get(Object instance, String field) {
         try {
-            return (R) fieldMap.computeIfAbsent(field, k -> getAccessibleField(clazz, field)).get(instance);
-        } catch (IllegalAccessException ex) {
-            throw new MinecraftReflectionException(ex);
+            return (R) fieldGetterMap.computeIfAbsent(field, k -> getFieldHandle(clazz, field, true)).invoke(instance);
+        } catch (Throwable th) {
+            throw new MinecraftReflectionException(th);
         }
     }
 
     public void set(Object instance, String field, Object value) {
         try {
-            fieldMap.computeIfAbsent(field, k -> getAccessibleField(clazz, field)).set(instance, value);
-        } catch (IllegalAccessException ex) {
-            throw new MinecraftReflectionException(ex);
+            fieldSetterMap.computeIfAbsent(field, k -> getFieldHandle(clazz, field, false)).invoke(instance, value);
+        } catch (Throwable th) {
+            throw new MinecraftReflectionException(th);
         }
     }
 
@@ -87,18 +90,18 @@ public class Reflection {
         try {
             Class<?>[] parameterTypes = ClassObject.getTypes(classObjects);
             Object[] parameters = ClassObject.getObjects(classObjects);
-            return (R) methodTree.computeIfAbsent(method, parameterTypes, types -> getAccessibleMethod(clazz, method, types)).invoke(instance, parameters);
-        } catch (IllegalAccessException | InvocationTargetException ex) {
-            throw new MinecraftReflectionException(ex);
+            return (R) methodTree.computeIfAbsent(method, parameterTypes, types -> getMethodHandle(clazz, method, types)).invoke(instance, parameters);
+        } catch (Throwable th) {
+            throw new MinecraftReflectionException(th);
         }
     }
 
     @SuppressWarnings("unchecked")
     public <R> R invoke(Object instance, String method, Object... parameters) {
         try {
-            return (R) methodTree.computeIfAbsent(method, getTypes(parameters), types -> getAccessibleMethod(clazz, method, types)).invoke(instance, parameters);
-        } catch (IllegalAccessException | InvocationTargetException ex) {
-            throw new MinecraftReflectionException(ex);
+            return (R) methodTree.computeIfAbsent(method, getTypes(parameters), types -> getMethodHandle(clazz, method, types)).invoke(instance, parameters);
+        } catch (Throwable th) {
+            throw new MinecraftReflectionException(th);
         }
     }
 
@@ -108,6 +111,14 @@ public class Reflection {
             types[i] = objects[i].getClass();
         }
         return types;
+    }
+
+    public static MethodHandle getConstructorHandle(Class<?> clazz, Class<?>... parameterTypes) {
+        try {
+            return MethodHandles.lookup().unreflectConstructor(getAccessibleConstructor(clazz, parameterTypes));
+        } catch (IllegalAccessException ex) {
+            throw new MinecraftReflectionException("Constructor '" + ReflectionStringUtils.constructor(clazz, parameterTypes) + "' could not be unreflected!", ex);
+        }
     }
 
     public static Constructor<?> getAccessibleConstructor(Class<?> clazz, Class<?>... parameterTypes) {
@@ -128,6 +139,19 @@ public class Reflection {
         }
     }
 
+    public static MethodHandle getFieldHandle(Class<?> clazz, String field, boolean isGetter) {
+        try {
+            Field f = getAccessibleField(clazz, field);
+            if (isGetter) {
+                return MethodHandles.lookup().unreflectGetter(f);
+            } else {
+                return MethodHandles.lookup().unreflectSetter(f);
+            }
+        } catch (IllegalAccessException ex) {
+            throw new MinecraftReflectionException("Field '" + ReflectionStringUtils.field(clazz, field) + "' could not be unreflected!", ex);
+        }
+    }
+
     public static Field getAccessibleField(Class<?> clazz, String field) {
         Field f = getField(clazz, field);
         if (!f.isAccessible()) f.setAccessible(true);
@@ -143,6 +167,14 @@ public class Reflection {
             } catch (NoSuchFieldException ex) {
                 throw new MinecraftReflectionException("Field '" + ReflectionStringUtils.field(clazz, field) + "' not found!", ex);
             }
+        }
+    }
+
+    public static MethodHandle getMethodHandle(Class<?> clazz, String method, Class<?>... parameterTypes) {
+        try {
+            return MethodHandles.lookup().unreflect(getAccessibleMethod(clazz, method, parameterTypes));
+        } catch (IllegalAccessException ex) {
+            throw new MinecraftReflectionException("Method '" + ReflectionStringUtils.method(clazz, method, parameterTypes) + "' could not be unreflected!", ex);
         }
     }
 
